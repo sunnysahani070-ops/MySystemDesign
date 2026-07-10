149cb7fbc18feb86c856a80d5d9c4bc23d195611
sunny <sunnysahani070@gmail.com>
2026-07-11 00:21:45 +0530

    Update elevator design pattern code with Observer pattern, display elements, and floor buttons

--- ElevatorDesignPattern/code.cpp
+++ ElevatorDesignPattern/code.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <algorithm>
 
 using namespace std;
 
-//====================== REQUEST ======================
-
-class Request {
-public:
-    virtual ~Request() {}
-};
+//=====================================================
+// ENUMS
+//=====================================================
 
 enum class Direction {
     UP,
     DOWN
+};
+
+// Forward Declarations
+class Elevator;
+class ElevatorController;
+
+//=====================================================
+// REQUEST
+//=====================================================
+
+class Request {
+public:
+    virtual ~Request() = default;
 };
 
 class ExternalRequest : public Request {
@@ -24,6 +35,14 @@ private:
 public:
     ExternalRequest(int floor, Direction direction)
         : floor(floor), direction(direction) {}
+
+    int getFloor() const {
+        return floor;
+    }
+
+    Direction getDirection() const {
+        return direction;
+    }
 };
 
 class InternalRequest : public Request {
@@ -33,16 +52,30 @@ private:
 public:
     InternalRequest(int destinationFloor)
         : destinationFloor(destinationFloor) {}
+
+    int getDestinationFloor() const {
+        return destinationFloor;
+    }
 };
 
-//====================== STATE PATTERN ======================
+//=====================================================
+// OBSERVER PATTERN
+//=====================================================
 
-class Elevator;
+class Observer {
+public:
+    virtual void update(Elevator* elevator) = 0;
+    virtual ~Observer() = default;
+};
+
+//=====================================================
+// STATE PATTERN
+//=====================================================
 
 class State {
 public:
     virtual void handle(Elevator* elevator) = 0;
-    virtual ~State() {}
+    virtual ~State() = default;
 };
 
 class IdleState : public State {
@@ -60,7 +93,65 @@ public:
     void handle(Elevator* elevator) override;
 };
 
-//====================== ELEVATOR ======================
+//=====================================================
+// BUTTONS
+//=====================================================
+
+class Button {
+public:
+    virtual void press() = 0;
+    virtual ~Button() = default;
+};
+
+class ExternalButton : public Button {
+protected:
+    int floorNumber;
+    ElevatorController* controller;
+
+public:
+    ExternalButton(int floorNumber, ElevatorController* controller)
+        : floorNumber(floorNumber),
+          controller(controller) {}
+};
+
+class UpButton : public ExternalButton {
+public:
+    UpButton(int floorNumber, ElevatorController* controller)
+        : ExternalButton(floorNumber, controller) {}
+
+    void press() override {
+        // TODO
+    }
+};
+
+class DownButton : public ExternalButton {
+public:
+    DownButton(int floorNumber, ElevatorController* controller)
+        : ExternalButton(floorNumber, controller) {}
+
+    void press() override {
+        // TODO
+    }
+};
+
+class InternalButton : public Button {
+private:
+    int destinationFloor;
+    Elevator* elevator;
+
+public:
+    InternalButton(int destinationFloor, Elevator* elevator)
+        : destinationFloor(destinationFloor),
+          elevator(elevator) {}
+
+    void press() override {
+        // TODO
+    }
+};
+
+//=====================================================
+// ELEVATOR
+//=====================================================
 
 class Elevator {
 private:
@@ -72,23 +163,41 @@ private:
     set<int> upRequests;
     set<int> downRequests;
 
+    set<Observer*> observers;
+
 public:
     Elevator(int id)
-        : id(id), currentFloor(0), currentState(nullptr) {}
+        : id(id),
+          currentFloor(0),
+          currentState(nullptr) {}
 
     void addRequest(Request* request) {
-        // Will implement later
+        // TODO
     }
 
     void move() {
-        // Will implement later
+        // TODO
+    }
+
+    void attachObserver(Observer* observer) {
+        observers.insert(observer);
+    }
+
+    void detachObserver(Observer* observer) {
+        observers.erase(observer);
+    }
+
+    void notifyObservers() {
+        for (Observer* observer : observers) {
+            observer->update(this);
+        }
     }
 
     int getCurrentFloor() const {
         return currentFloor;
     }
 
-    State* getState() const {
+    State* getCurrentState() const {
         return currentState;
     }
 
@@ -97,21 +206,43 @@ public:
     }
 };
 
-//====================== STATE IMPLEMENTATION ======================
+//=====================================================
+// OBSERVERS
+//=====================================================
+
+class CabinDisplay : public Observer {
+public:
+    void update(Elevator* elevator) override {
+        cout << "Cabin Display Updated\n";
+    }
+};
+
+class FloorDisplay : public Observer {
+public:
+    void update(Elevator* elevator) override {
+        cout << "Floor Display Updated\n";
+    }
+};
+
+//=====================================================
+// STATE IMPLEMENTATION
+//=====================================================
 
 void IdleState::handle(Elevator* elevator) {
-    cout << "Elevator is idle\n";
+    cout << "Idle State\n";
 }
 
 void MovingUpState::handle(Elevator* elevator) {
-    cout << "Elevator moving up\n";
+    cout << "Moving Up\n";
 }
 
 void MovingDownState::handle(Elevator* elevator) {
-    cout << "Elevator moving down\n";
+    cout << "Moving Down\n";
 }
 
-//====================== STRATEGY PATTERN ======================
+//=====================================================
+// STRATEGY PATTERN
+//=====================================================
 
 class DispatchStrategy {
 public:
@@ -119,7 +250,7 @@ public:
         vector<Elevator*>& elevators,
         ExternalRequest* request) = 0;
 
-    virtual ~DispatchStrategy() {}
+    virtual ~DispatchStrategy() = default;
 };
 
 class NearestStrategy : public DispatchStrategy {
@@ -129,7 +260,6 @@ public:
         ExternalRequest* request) override {
 
         cout << "Nearest Strategy\n";
-
         return nullptr;
     }
 };
@@ -141,12 +271,13 @@ public:
         ExternalRequest* request) override {
 
         cout << "Least Loaded Strategy\n";
-
         return nullptr;
     }
 };
 
-//====================== CONTROLLER ======================
+//=====================================================
+// CONTROLLER
+//=====================================================
 
 class ElevatorController {
 private:
@@ -154,9 +285,11 @@ private:
     DispatchStrategy* strategy;
 
 public:
-    ElevatorController(vector<Elevator*> elevators,
-                       DispatchStrategy* strategy)
-        : elevators(elevators), strategy(strategy) {}
+    ElevatorController(
+        vector<Elevator*> elevators,
+        DispatchStrategy* strategy)
+        : elevators(elevators),
+          strategy(strategy) {}
 
     void handleExternalRequest(ExternalRequest* request) {
 
@@ -173,7 +306,42 @@ public:
     }
 };
 
-//====================== BUILDING ======================
+//=====================================================
+// FLOOR
+//=====================================================
+
+class Floor {
+private:
+    int floorNumber;
+
+    UpButton* upButton;
+    DownButton* downButton;
+
+public:
+    Floor(int floorNumber,
+          ElevatorController* controller)
+        : floorNumber(floorNumber) {
+
+        upButton = new UpButton(floorNumber, controller);
+        downButton = new DownButton(floorNumber, controller);
+    }
+
+    int getFloorNumber() const {
+        return floorNumber;
+    }
+
+    void pressUpButton() {
+        upButton->press();
+    }
+
+    void pressDownButton() {
+        downButton->press();
+    }
+};
+
+//=====================================================
+// BUILDING
+//=====================================================
 
 class Building {
 private:
@@ -184,30 +352,19 @@ private:
     ElevatorController* controller;
 
 public:
-    Building(int floors,
+    Building(int totalFloors,
              vector<Elevator*> elevators,
              ElevatorController* controller)
-        : totalFloors(floors),
+        : totalFloors(totalFloors),
           elevators(elevators),
           controller(controller) {}
 };
 
-//====================== MAIN ======================
+//=====================================================
+// MAIN
+//=====================================================
 
 int main() {
-
-    Elevator* e1 = new Elevator(1);
-    Elevator* e2 = new Elevator(2);
-    Elevator* e3 = new Elevator(3);
-
-    vector<Elevator*> elevators = {e1, e2, e3};
-
-    DispatchStrategy* strategy = new NearestStrategy();
-
-    ElevatorController* controller =
-        new ElevatorController(elevators, strategy);
-
-    Building building(10, elevators, controller);
 
     return 0;
 }
